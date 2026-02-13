f9c6b476837a017d6315b8a6d9380a4a057e69c0
Yuvarajkurri <kurriyuvaraj13@gmail.com>
2026-02-14 01:06:02 +0530

    HomePage Folder Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import PopularDestinations from './components/PopularDestinations'
-import TravelStyles from './components/TravelStyles'
-import WhyChooseUs from './components/WhyChooseUs'
-import HeroSection from './components/HeroSection'
+import Home from './pages/home'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div style={{ padding: '0', flex: 1, width: '100%' }}>
-        <HeroSection />
-        <PopularDestinations />
-        <TravelStyles />
-        <WhyChooseUs />
-      </div>
-      <Footer />
+       <Home />
+
+
+
+       
     </>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import TravelStyles from '../components/TravelStyles';
-
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import PopularDestinations from '../components/homePageComponents/PopularDestinations'
+import WhyChooseUs from '../components/homePageComponents/WhyChooseUs'
+import HeroSection from '../components/homePageComponents/HeroSection'
 const Home = () => {
     return (
         <div>
             <Navbar />
-            <TravelStyles />
+            <HeroSection />
+            <PopularDestinations />
+            <WhyChooseUs />
+            <Footer />
         </div>
     );
 };
