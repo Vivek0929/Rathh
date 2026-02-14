afcd25e19cb7ebf589852ed3ca4d3c0e70759489
Yuvarajkurri <kurriyuvaraj13@gmail.com>
2026-02-15 00:53:44 +0530

    Taverl Setion Added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,12 +4,14 @@ import Footer from '../components/Footer'
 import PopularDestinations from '../components/homePageComponents/PopularDestinations'
 import WhyChooseUs from '../components/homePageComponents/WhyChooseUs'
 import HeroSection from '../components/homePageComponents/HeroSection'
+import TravelStyles from '../components/homePageComponents/TravelStyles';
 const Home = () => {
     return (
         <div>
             <Navbar />
             <HeroSection />
             <PopularDestinations />
+            <TravelStyles />
             <WhyChooseUs />
             <Footer />
         </div>
