ba1617fc7a5d7bdff174884e0b7ac54e04da4a4a
Yuvarajkurri <kurriyuvaraj13@gmail.com>
2026-02-23 18:01:34 +0530

    BrowserRoter added

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import './App.css'
 import Home from './pages/home';
 import ContactUs from './pages/ContactUs';
 import Dyna from './pages/Dyna';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 
 
 function App() {
   return (
-    <Router>
+  <BrowserRouter> 
+   <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/dyna" element={<Dyna />} />
       </Routes>
     </Router>
+    </BrowserRouter>
   )
 }
 
